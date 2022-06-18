3fe81093e1e27cdbe9c235d9a7870b994e28f557
Mohammad Fayed <mohammadfayed97@gmail.com>
2022-06-18 05:00:30 +0200

    add support for custmaize row and cell

---
+++ src/SmartUI.Grid/GridRowSetting.cs
@@ -0,0 +1,25 @@
+﻿namespace SmartUI.Grid
+{
+    using System.Text;
+
+    public class GridRowSetting
+    {
+        private string[] _cssClass;
+        private StringBuilder _text = new StringBuilder();
+        public void AddClass(string[] cssClass) => _cssClass = cssClass;
+
+        internal string GetCssClass()
+        {
+            if (_cssClass == null)
+                return string.Empty;
+
+            _text.Clear();
+            foreach (var item in _cssClass)
+            {
+                _text.Append(item);
+                _text.Append(" ");
+            }
+            return _text.ToString();
+        }
+    }
+}
---
+++ src/SmartUI.Grid/QueryCellInfoEventArgs.cs
@@ -0,0 +1,16 @@
+﻿namespace SmartUI.Grid
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class QueryCellInfoEventArgs<TModel>
+    {
+        public TModel Data { get; private set; }
+        public GridRowSetting Row { get; set; } = new GridRowSetting();
+        public QueryCellInfoEventArgs(TModel data) => Data = data;
+
+    }
+}
